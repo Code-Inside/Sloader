48bff3f03c87518b1d342ad470289e91c5addd27
Robert Muehsig <robert.muehsig@code-developer.de>
2014-12-30 15:56:33 +0100

    test draft

---
+++ tests/Sloader.Tests/TestHelperForCurrentProject.cs
@@ -0,0 +1,36 @@
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace Sloader.Tests
+{
+    public static class TestHelperForCurrentProject
+    {
+        private const string ResourcePath = "Sloader.Tests.{0}";
+
+        public static Stream GetTestFileStream(string folderAndFileInProjectPath)
+        {
+            var asm = Assembly.GetExecutingAssembly();
+            var resource = string.Format(ResourcePath, folderAndFileInProjectPath);
+
+            return asm.GetManifestResourceStream(resource);
+        }
+
+        public static string GetTestFileContent(string folderAndFileInProjectPath)
+        {
+            var asm = Assembly.GetExecutingAssembly();
+            var resource = string.Format(ResourcePath, folderAndFileInProjectPath);
+
+            using (var stream = asm.GetManifestResourceStream(resource))
+            {
+                if (stream != null)
+                {
+                    var reader = new StreamReader(stream);
+                    return reader.ReadToEnd();
+                }
+            }
+            return String.Empty;
+        }
+
+    }
+}
