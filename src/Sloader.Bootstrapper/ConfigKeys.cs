c4a56a670bd3f1ac9bdc147e013bf1393859d438
Robert Muehsig <robert.muehsig@code-developer.de>
2014-12-30 14:49:22 +0100

    strongly typed configkeys

---
+++ src/Sloader.Bootstrapper/ConfigKeys.cs
@@ -0,0 +1,9 @@
+namespace Sloader.Bootstrapper
+{
+    public static class ConfigKeys
+    {
+        public static string SecretTwitterConsumerKey = "Sloader.SecretTwitterConsumerKey";
+        public static string SecretTwitterConsumerSecret = "Sloader.SecretTwitterConsumerSecret";
+        public static string MasterCrawlerConfigPath = "Sloader.MasterCrawlerConfigPath";
+    }
+}
--- src/Sloader.Bootstrapper/Program.cs
+++ src/Sloader.Bootstrapper/Program.cs
@@ -16,8 +16,6 @@ namespace Sloader.Bootstrapper
 {
     public class Program
     {
-
-
         public static void Main()
         {
             Trace.TraceInformation("Crawler Console App started.");
@@ -45,13 +43,13 @@ namespace Sloader.Bootstrapper
 #if DEBUG
             var configString = await client.GetStringAsync("https://raw.githubusercontent.com/Code-Inside/Hub/master/CrawlerConfig.json");
 #else
-            var configString = await client.GetStringAsync(ConfigurationManager.AppSettings["MasterCrawlerJsonConfigPath"]);
+            var configString = await client.GetStringAsync(ConfigurationManager.AppSettings[ConfigKeys.MasterCrawlerConfigPath]);
 #endif
             var config = JsonConvert.DeserializeObject<MasterCrawlerConfig>(configString);
 
             var secrets = new MasterCrawlerSecrets();
-            secrets.TwitterConsumerKey = ConfigurationManager.AppSettings["TwitteroAuthConsumerKey"];
-            secrets.TwitterConsumerSecret = ConfigurationManager.AppSettings["TwitteroAuthConsumerSecret"];
+            secrets.TwitterConsumerKey = ConfigurationManager.AppSettings[ConfigKeys.SecretTwitterConsumerKey];
+            secrets.TwitterConsumerSecret = ConfigurationManager.AppSettings[ConfigKeys.SecretTwitterConsumerSecret];
             var crawler = new MasterCrawler(config, secrets);
 
             return await crawler.RunAllCrawlers();
