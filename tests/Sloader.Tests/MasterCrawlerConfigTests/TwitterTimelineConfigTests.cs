2fe4fdacfb707169464998e4ff3a98bab22f12d7
Robert Muehsig <robert.muehsig@code-developer.de>
2015-02-13 23:14:10 +0100

    twittertimelinecrawlertests added

---
+++ tests/Sloader.Tests/MasterCrawlerConfigTests/TwitterTimelineConfigTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sloader.Shared;
+using Xunit;
+using YamlDotNet.Serialization;
+
+namespace Sloader.Tests.MasterCrawlerConfigTests
+{
+    public class TwitterTimelineConfigTests
+    {
+      
+        [Fact]
+        public async Task Handle_Is_Identifier()
+        {
+            StringBuilder fakeYaml = new StringBuilder();
+            fakeYaml.AppendLine("TwitterTimelinesToCrawl:");
+            fakeYaml.AppendLine("- Handle: codeinsideblog");
+
+            var deserializer = new Deserializer();
+            var result = deserializer.Deserialize<MasterCrawlerConfig>(new StringReader(fakeYaml.ToString()));
+            var configValue = result.TwitterTimelinesToCrawl.First();
+
+            Assert.Equal("codeinsideblog", configValue.ResultIdentifier);
+        }
+    }
+}
