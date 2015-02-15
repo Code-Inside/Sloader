6465a523a08cb46b6bbf9864dfc0882c8dcfc69a
Robert Muehsig <robert.muehsig@code-developer.de>
2015-02-15 16:40:34 +0100

    first steps with nuget pack

--- build/build.fsx
+++ build/build.fsx
@@ -5,33 +5,52 @@ open Fake
 RestorePackages()
 
 // Properties
-let buildDir = "./artifacts/build/"
-let testsDir  = "./artifacts/tests/"
+let artifactsDir = @".\artifacts\"
+let artifactsBuildDir = "./artifacts/build/"
+let artifactsTestsDir  = "./artifacts/tests/"
+let artifactsNugetDir  = @".\artifacts\nuget\"
+let toolNugetExe = @".nuget\nuget.exe"
 
 // Targets
 Target "Clean" (fun _ ->
     trace "Cleanup..."
-    CleanDirs [buildDir; testsDir]
+    CleanDirs [artifactsDir]
 )
 
 Target "BuildApp" (fun _ ->
    trace "Building App..."
    !! "src/**/*.csproj"
-     |> MSBuildRelease buildDir "Build"
+     |> MSBuildRelease artifactsBuildDir "Build"
      |> Log "AppBuild-Output: "
 )
 
 Target "BuildTests" (fun _ ->
     trace "Building Tests..."
     !! "tests/**/*.csproj"
-      |> MSBuildDebug testsDir "Build"
+      |> MSBuildDebug artifactsTestsDir "Build"
       |> Log "TestBuild-Output: "
 )
 
 Target "RunTests" (fun _ ->
     trace "Running Tests..."
-    !! (testsDir + @"\*Tests.dll") 
-      |> xUnit (fun p -> {p with OutputDir = testsDir })
+    !! (artifactsTestsDir + @"\*Tests.dll") 
+      |> xUnit (fun p -> {p with OutputDir = artifactsTestsDir })
+)
+
+
+
+let nuspec = @".\src\Sloader.Types\Sloader.Types.nuspec"
+// Poor mans NuGet Pack Solution for FAKE...  
+Target "CreateNuGetPackages" (fun _ ->
+    trace "Create NuGet Packages..."
+    CreateDir artifactsNugetDir
+    let result =
+        ExecProcess (fun info -> 
+            info.FileName <- toolNugetExe
+            info.Arguments <- "pack " + nuspec + " -OutputDirectory " + artifactsNugetDir
+        ) (System.TimeSpan.FromMinutes 1.)
+ 
+    if result <> 0 then failwith "Failed result from NuGet"
 )
 
 Target "Default" (fun _ ->
@@ -43,6 +62,7 @@ Target "Default" (fun _ ->
   ==> "BuildApp"
   ==> "BuildTests"
   ==> "RunTests"
+  ==> "CreateNuGetPackages"
   ==> "Default"
 
 // start build
