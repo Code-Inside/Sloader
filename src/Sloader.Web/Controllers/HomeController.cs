59f349b4676aed022f668edb2ff12bc4e409d58c
Robert Muehsig <robert.muehsig@code-developer.de>
2015-02-19 22:26:29 +0100

    draft controller

--- src/Sloader.Web/Controllers/HomeController.cs
+++ src/Sloader.Web/Controllers/HomeController.cs
@@ -1,15 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using Sloader.Crawler.Config;
+using Sloader.Web.Models;
+using YamlDotNet.Serialization;
 
 namespace Sloader.Web.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public async Task<ActionResult> Index()
         {
+            var viewModel = new HomeIndexViewModel();
+            viewModel.MasterCrawlerConfigPath = ConfigurationManager.AppSettings[ConfigKeys.MasterCrawlerConfigPath];
+            if (viewModel.MasterCrawlerConfigPath == null)
+            {
+                viewModel.MasterCrawlerConfigIsReadable = false;
+            }
+            else
+            {
+                var client = new HttpClient();
+
+                var configString = await client.GetStringAsync(ConfigurationManager.AppSettings[ConfigKeys.MasterCrawlerConfigPath]);
+
+                var deserializer = new Deserializer();
+                viewModel.MasterCrawlerConfig = deserializer.Deserialize<MasterCrawlerConfig>(new StringReader(configString));
+
+                var secrets = new MasterCrawlerSecrets();
+                secrets.TwitterConsumerKey = ConfigurationManager.AppSettings[ConfigKeys.SecretTwitterConsumerKey];
+                secrets.TwitterConsumerSecret = ConfigurationManager.AppSettings[ConfigKeys.SecretTwitterConsumerSecret];
+
+            }
+
+
             return View();
         }
 
---
+++ src/Sloader.Web/Models/HomeIndexViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sloader.Web.Models
+{
+    public class HomeIndexViewModel
+    {
+        public string MasterCrawlerConfigPath { get; set; }
+        public bool MasterCrawlerConfigIsReadable { get; set; }
+
+    }
+}
