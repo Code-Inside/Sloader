893af90f435568e26df8d573ae84714e3262720b
Robert Muehsig <robert.muehsig@code-developer.de>
2015-02-19 22:19:17 +0100

    remove contact stuff

--- src/Sloader.Web/Controllers/HomeController.cs
+++ src/Sloader.Web/Controllers/HomeController.cs
@@ -20,11 +20,5 @@ namespace Sloader.Web.Controllers
             return View();
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
