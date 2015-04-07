d3ccd48e89182bd88e7bf636e4fb0f4030607d8c
Robert Muehsig <robert.muehsig@code-developer.de>
2015-04-08 00:26:25 +0200

    sloaderrunner

--- src/Sloader.Crawler/SloaderRunner.cs
+++ src/Sloader.Crawler/SloaderRunner.cs
@@ -9,12 +9,12 @@ using Sloader.Results;
 
 namespace Sloader.Crawler
 {
-    public class MasterCrawler
+    public class SloaderRunner
     {
-        private readonly MasterCrawlerConfig _config;
-        private readonly MasterCrawlerSecrets _secrets;
+        private readonly SloaderConfig _config;
+        private readonly SloaderSecrets _secrets;
 
-        public MasterCrawler(MasterCrawlerConfig config, MasterCrawlerSecrets secrets)
+        public SloaderRunner(SloaderConfig config, SloaderSecrets secrets)
         {
             _config = config;
             _secrets = secrets;
@@ -27,10 +27,13 @@ namespace Sloader.Crawler
 
             var crawlerRunResult = new CrawlerRun();
 
+            if (_config.Crawler == null)
+                return crawlerRunResult;
+
             // Feeds
-            if (_config.FeedsToCrawl.Any())
+            if (_config.Crawler.FeedsToCrawl.Any())
             {
-                foreach (var feed in _config.FeedsToCrawl)
+                foreach (var feed in _config.Crawler.FeedsToCrawl)
                 {
                     var feedCrawler = new FeedCrawler();
                     var feedResult = await feedCrawler.DoWorkAsync(feed);
@@ -40,13 +43,13 @@ namespace Sloader.Crawler
             }
 
             // Tweets
-            if (_config.TwitterTimelinesToCrawl.Any() && _secrets.IsTwitterConsumerConfigured)
+            if (_config.Crawler.TwitterTimelinesToCrawl.Any() && _secrets.IsTwitterConsumerConfigured)
             {
                 ITwitterOAuthTokenService oAuthTokenLoader = new TwitterOAuthTokenService();
                 var oauth = await oAuthTokenLoader.GetAsync(_secrets.TwitterConsumerKey, _secrets.TwitterConsumerSecret);
                 if (string.IsNullOrWhiteSpace(oauth) == false)
                 {
-                    foreach (var handle in _config.TwitterTimelinesToCrawl)
+                    foreach (var handle in _config.Crawler.TwitterTimelinesToCrawl)
                     {
                         var twitterCrawler = new TwitterTimelineCrawler();
                         twitterCrawler.OAuthToken = oauth;
