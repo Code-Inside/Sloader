1520ded3e8b25a936f367b5dc66149bdd1c4d25a
Robert Muehsig <robert.muehsig@codeinside.eu>
2017-03-07 21:42:51 +0100

    init doc drops...

--- src/Sloader.Config/Drop/File/FileDropConfig.cs
+++ src/Sloader.Config/Drop/File/FileDropConfig.cs
@@ -1,7 +1,20 @@
 namespace Sloader.Config.Drop.File
 {
+    /// <summary>
+    /// Drops the output as a File.
+    /// </summary>
+    /// <example>
+    /// Demo yml config:
+    /// <code>
+    /// FileDrops:
+    /// - FilePath: "test.json"
+    /// </code>
+    /// </example>
     public class FileDropConfig 
     {
+        /// <summary>
+        /// Relative or absolute path, e.g. test.json or D:/Temp/test.json
+        /// </summary>
         public string FilePath { get; set; }
     }
 }
