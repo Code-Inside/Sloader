854351de68cb22c6d797c739c94557850628b3b1
Robert Muehsig <robert.muehsig@codeinside.eu>
2017-04-10 22:04:24 +0200

    documentation

--- src/Sloader.Engine/Crawler/Twitter/TwitterUserCrawler.cs
+++ src/Sloader.Engine/Crawler/Twitter/TwitterUserCrawler.cs
@@ -8,27 +8,48 @@ using System.Threading.Tasks;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Sloader.Config.Crawler.Twitter;
-using Sloader.Result;
+using Sloader.Engine.Crawler.DependencyServices;
 using Sloader.Result.Types;
 using WorldDomination.Net.Http;
 
 namespace Sloader.Engine.Crawler.Twitter
 {
+    /// <summary>
+    /// Crawler for the Twitter User Lookup API:
+    /// <para>https://dev.twitter.com/rest/reference/get/users/lookup</para>
+    /// </summary>
     public class TwitterUserCrawler : ICrawler<TwitterUserResult, TwitterUserCrawlerConfig>
     {
         private readonly HttpClient _httpClient;
 
+        /// <summary>
+        /// Ctor with default dependencies
+        /// </summary>
         public TwitterUserCrawler()
         {
             _httpClient = new HttpClient();
         }
 
+        /// <summary>
+        /// Ctor for testing
+        /// </summary>
+        /// <param name="messageHandler">HttpMessageHandler to simulate any HTTP response</param>
         public TwitterUserCrawler(FakeHttpMessageHandler messageHandler)
         {
             _httpClient = new HttpClient(messageHandler);
         }
+
+        /// <summary>
+        /// Needed Token to consume the API
+        /// </summary>
+        /// <see cref="TwitterOAuthTokenService"/>
         public string OAuthToken { get; set; }
 
+        /// <summary>
+        /// Actual worker method which will load the user information for the given users.
+        /// </summary>
+        /// <param name="config">Twitter Screenname etc.</param>
+        /// <returns>result for given config data (e.g. follower count etc.)</returns>
         public async Task<TwitterUserResult> DoWorkAsync(TwitterUserCrawlerConfig config)
         {
             if (string.IsNullOrWhiteSpace(config.Handle))
