90f49148cbb95a7a2dd9e1f1dd26f4bb153940cd
Robert Muehsig <robert.muehsig@codeinside.eu>
2017-04-13 22:14:43 +0200

    docs and cleanup

--- src/Sloader.Result/BaseResult.cs
+++ src/Sloader.Result/BaseResult.cs
@@ -3,8 +3,15 @@ using Newtonsoft.Json.Converters;
 
 namespace Sloader.Result
 {
+    /// <summary>
+    /// Base type for all results
+    /// </summary>
     public abstract class BaseResult
     {
+        /// <summary>
+        /// This property will tell clients the exact result type.
+        /// <para>Each result type has different properties, so use this to determine which property is available.</para>
+        /// </summary>
         [JsonConverter(typeof(StringEnumConverter))]
         public abstract KnownResultType ResultType { get; }
     }
--- src/Sloader.Result/CrawlerRun.cs
+++ src/Sloader.Result/CrawlerRun.cs
@@ -5,17 +5,39 @@ using Newtonsoft.Json;
 
 namespace Sloader.Result
 {
+    /// <summary>
+    /// Container class which stores some common "run" data & the actual result collection.
+    /// </summary>
     public class CrawlerRun
     {
+        /// <summary>
+        /// Ctor
+        /// </summary>
         public CrawlerRun()
         {
             Data = new Dictionary<string, BaseResult>();
         }
+
+        /// <summary>
+        /// Actual result data as Dictionary with the given key for the given result.
+        /// </summary>
         public Dictionary<string, BaseResult> Data { get; }
 
+        /// <summary>
+        /// Stores the sloader "run" datetime
+        /// </summary>
         public DateTime RunOn { get; set; }
+
+        /// <summary>
+        /// Stores the loading or "run" time in ms
+        /// </summary>
         public long RunDurationInMilliseconds { get; set; }
 
+        /// <summary>
+        /// Helper method to ensure that each key is only set once in the result Data
+        /// </summary>
+        /// <param name="key">Desired key</param>
+        /// <param name="data">Any BaseResult for this Key</param>
         public void AddResultDataPair(string key, BaseResult data)
         {
             Trace.TraceInformation($"{nameof(AddResultDataPair)} : '{key}'");
@@ -25,6 +47,10 @@ namespace Sloader.Result
             }
         }
 
+        /// <summary>
+        /// Serializes the object via JSON.NET
+        /// </summary>
+        /// <returns>Large JSON with "run" data & actual result data.</returns>
         public string ToJson()
         {
             return JsonConvert.SerializeObject(this);
