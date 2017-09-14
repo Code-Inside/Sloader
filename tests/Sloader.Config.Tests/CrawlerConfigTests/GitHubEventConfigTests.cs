c52c4ebca8656eb4f4333618516eef92ca450ed9
Robert Muehsig <robert.muehsig@codeinside.eu>
2017-09-14 22:46:25 +0200

    config part for github filtering

--- tests/Sloader.Config.Tests/CrawlerConfigTests/GitHubEventConfigTests.cs
+++ tests/Sloader.Config.Tests/CrawlerConfigTests/GitHubEventConfigTests.cs
@@ -18,6 +18,7 @@ namespace Sloader.Config.Tests.CrawlerConfigTests
             fakeYaml.AppendLine("    User: robertmuehsig");
             fakeYaml.AppendLine("    Repository: code-inside/sloader");
             fakeYaml.AppendLine("    Key: Test");
+            fakeYaml.AppendLine("    Events: [PullRequestEvent, IssuesEvent, ReleaseEvent]");
 
             var deserializer = Constants.SloaderYamlDeserializer;
             var result = deserializer.Deserialize<SloaderConfig>(new StringReader(fakeYaml.ToString()));
@@ -27,7 +28,30 @@ namespace Sloader.Config.Tests.CrawlerConfigTests
             Assert.Equal("codeinside", configValue.Organization);
             Assert.Equal("robertmuehsig", configValue.User);
             Assert.Equal("code-inside/sloader", configValue.Repository);
+            Assert.True(configValue.Events.Count == 3);
+            Assert.True(configValue.Events.Contains("PullRequestEvent"));
+            Assert.True(configValue.Events.Contains("IssuesEvent"));
+            Assert.True(configValue.Events.Contains("ReleaseEvent"));
+        }
 
+        [Fact]
+        public void Can_Convert_With_StandardEvents()
+        {
+            StringBuilder fakeYaml = new StringBuilder();
+            fakeYaml.AppendLine("Crawler:");
+            fakeYaml.AppendLine("  GitHubEventsToCrawl:");
+            fakeYaml.AppendLine("  - Organization: codeinside");
+            fakeYaml.AppendLine("    User: robertmuehsig");
+            fakeYaml.AppendLine("    Repository: code-inside/sloader");
+            fakeYaml.AppendLine("    Key: Test");
+
+            var deserializer = Constants.SloaderYamlDeserializer;
+            var result = deserializer.Deserialize<SloaderConfig>(new StringReader(fakeYaml.ToString()));
+            var configValue = result.Crawler.GitHubEventsToCrawl.First();
+
+            Assert.True(configValue.Events.Count == 2);
+            Assert.True(configValue.Events.Contains("PullRequestEvent"));
+            Assert.True(configValue.Events.Contains("IssuesEvent"));
         }
     }
 }
