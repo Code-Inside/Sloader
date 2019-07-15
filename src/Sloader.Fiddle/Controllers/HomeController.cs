6b675036fb8ab1c0f6f43b395756b27671540718
Robert Muehsig <robert.muehsig@codeinside.eu>
2019-07-15 23:27:51 +0200

    draft

--- src/Sloader.Fiddle/Controllers/HomeController.cs
+++ src/Sloader.Fiddle/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Sloader.Engine;
 using Sloader.Fiddle.Models;
 
 namespace Sloader.Fiddle.Controllers
@@ -12,7 +13,30 @@ namespace Sloader.Fiddle.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            HomeViewModel viewModel = new HomeViewModel();
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Fiddle(HomeViewModel viewModel)
+        {
+            try
+            {
+                var runner = new SloaderRunner(Sloader.Config.SloaderConfig.Parse(viewModel.Input, new Dictionary<string, string>()));
+                var crawlerRun = await runner.RunAllCrawlers();
+
+                var json = crawlerRun.ToJson();
+                viewModel.Output = json;
+            }
+            catch(Exception exc)
+            {
+                viewModel.HasError = true;
+                viewModel.Output = exc.Message;
+            }
+
+            ModelState.Clear();
+
+            return View("Index", viewModel);
         }
 
         public IActionResult Privacy()
---
+++ src/Sloader.Fiddle/Models/HomeViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sloader.Fiddle.Models
+{
+    public class HomeViewModel
+    {
+        public string Input { get; set; }
+        public string Output { get; set; }
+        public bool HasError { get; set; }
+    }
+}
